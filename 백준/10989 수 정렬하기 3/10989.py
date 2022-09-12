769cf466fd78d1ee7c0d36e1efd005cd64b34a24
cmkds <dongsu712@naver.com>
2022-09-12 23:33:44 +0900

    0912

---
+++ 백준/10989 수 정렬하기 3/10989.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt')
+
+n = int(sys.stdin.readline())
+lst = [0] * 10001
+
+for i in range(n):
+    lst[int(sys.stdin.readline())] += 1
+
+for i in range(1,10001):
+    if lst[i] != 0:
+        for j in range(lst[i]):
+            print(i)
