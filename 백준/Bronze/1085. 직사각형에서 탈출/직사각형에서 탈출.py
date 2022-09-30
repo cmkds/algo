b5a1df0c79354dc82c8855cda4fe93bd6faa3caf
cmkds <107685502+cmkds@users.noreply.github.com>
2022-09-30 10:51:11 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,8 +1,3 @@
-x, y, w, h = map(int,input().split())
+x, y, w, h = map(int, input().split())
 
-#가로 계산
-garo = min(w-x,x-0)
-#세로 계산
-sero = min(h-y,y-0)
-
-print(min(garo,sero))
+print(min(w-x,h-y,x,y))
