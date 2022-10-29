88e829955d3ae266e98b4967307a1927125cb099
cmkds <dongsu712@naver.com>
2022-10-29 23:49:36 +0900

    1029

---
+++ 백준/class2/7568덩치.py
@@ -0,0 +1,13 @@
+n = int(input())
+lst = []
+
+for _ in range(n):
+    weight, height = map(int,input().split())
+    lst.append((weight,height))
+
+for i in lst:
+    rank = 1
+    for j in lst:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=' ')
